e5aae68f0bf91fef57431b2494542e9223585262
digaodev <rodrigo.rb.dev@gmail.com>
2018-07-22 19:52:07 -0300

    Feature: restructure and add styles to ExpenseList and ExpenseListItem components.

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -3,13 +3,28 @@ import React from 'react';
 import ExpenseListItem from './ExpenseListItem';
 
 const ExpenseList = ({ expenses }) => (
-  <div>
-    <h1>ExpenseList</h1>
-    <ul>
-      {expenses.map(expense => (
-        <ExpenseListItem key={expense.id} {...expense} />
-      ))}
-    </ul>
+  <div className="content-container">
+    <div className="list-header">
+      <div className="show-for-mobile">Expenses</div>
+      <div className="show-for-desktop">Expense</div>
+      <div className="show-for-desktop">Amount</div>
+    </div>
+
+    <div className="list-body">
+      {expenses.length === 0 ? (
+        <div>
+          <span className="list-item list-item--message">
+            You have no expenses to list
+          </span>
+        </div>
+      ) : (
+        <ul>
+          {expenses.map(expense => (
+            <ExpenseListItem key={expense.id} {...expense} />
+          ))}
+        </ul>
+      )}
+    </div>
   </div>
 );
 
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -13,12 +13,14 @@ const ExpenseListItem = ({
   handleRemove
 }) => (
   <li>
-    <Link to={`/edit/${id}`}>
+    <Link className="list-item" to={`/edit/${id}`}>
       <div>
-        <h3>Description: {description}</h3>
-        <p>Amount: {numeral(amount / 100).format('$0,0.00')}</p>
-        <time>Created: {moment(createdAt).format('MMMM Do, YYYY')}</time>
+        <h3 className="list-item__title">{description}</h3>
+        <span className="list-item__subtitle">
+          <time>Created on {moment(createdAt).format('MMMM Do, YYYY')}</time>
+        </span>
       </div>
+      <h3 className="list-item__data">{numeral(amount / 100).format('$0,0.00')}</h3>
     </Link>
   </li>
 );
